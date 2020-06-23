6076bacf5628162082e43496fbee8887c3f325f6
jtbc <jtbc@naver.com>
2020-06-24 07:09:52 +0900

    feat : Add example codes
    함수 예제 코드 추가

--- Lecture_C/Lecture_C/function.c
+++ Lecture_C/Lecture_C/function.c
@@ -2,36 +2,45 @@
 
 // 선언
 void p(int num);
+void function_without_return();
+int function_with_return();
 
-int main(void)
+int main_function(void)
 {
 	// function
 	// 계산기
 
-	int num = 2;
-	//printf("num은 %d 입니다.\n", num); // 2
-	p(num);
+	//int num = 2;
+	////printf("num은 %d 입니다.\n", num); // 2
+	//p(num);
 
-	// 2 + 3은?
-	num = num + 3;
-	//printf("num은 %d 입니다.\n", num); // 5
-	p(num);
+	//// 2 + 3은?
+	//num = num + 3;
+	////printf("num은 %d 입니다.\n", num); // 5
+	//p(num);
 
 
-	// 5 - 1은?
-	num -= 1; // num = num - 1;
-	//printf("num은 %d 입니다.\n", num); // 4
-	p(num);
+	//// 5 - 1은?
+	//num -= 1; // num = num - 1;
+	////printf("num은 %d 입니다.\n", num); // 4
+	//p(num);
 
-	// 4 x 3은?
-	num *= 3;
-	//printf("num은 %d 입니다.\n", num); // 12
-	p(num);
+	//// 4 x 3은?
+	//num *= 3;
+	////printf("num은 %d 입니다.\n", num); // 12
+	//p(num);
 
-	// 12 / 6은?
-	num /= 6;
-	//printf("num은 %d 입니다.\n", num); // 2
-	p(num); // 얘가 위 문장을 수행해주는 함수.
+	//// 12 / 6은?
+	//num /= 6;
+	////printf("num은 %d 입니다.\n", num); // 2
+	//p(num); // 얘가 위 문장을 수행해주는 함수.
+
+	// 반환값이 없는 함수
+	function_without_return();
+
+	// 반환값이 있는 함수 
+	int ret = function_with_return();
+	p(ret);
 
 	return 0;
 }
@@ -40,4 +49,28 @@ int main(void)
 void p(int num)
 {
 	printf("num은 %d 입니다.\n", num);
+}
+
+// 전달값 : 56
+// 함수 : ㅁ + 4
+// 반환형 : ?
+
+//반환형 함수이름(전달값)
+// int 함수이름(num)
+//{
+//	return num + 4;
+//}
+// void 함수이름(int num1, int num2, char c, float f)
+//{
+//}
+
+void function_without_return() 
+{
+	printf("반환값이 없는 함수입니다.\n");
+}
+
+int function_with_return()
+{
+	printf("반환값이 있는 함수입니다.\n");
+	return 10;
 }
